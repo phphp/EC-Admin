d0e8b35361a20cafb599f838741538e5a99bf453
sun <sun@phphp.net>
2019-08-27 13:42:58 +0800

    设置手风琴开闭状态

--- resources/views/admin/templates/side_nav.blade.php
+++ resources/views/admin/templates/side_nav.blade.php
@@ -3,14 +3,14 @@
 <a class="nav-link active" href="{{ route('dashboard') }}"><i class="iconfont icon-home"></i> 控制台</a>
 
 <a class="nav-link" data-toggle="collapse" href="#nav-admin"><i class="iconfont icon-user-group-fill"></i> 管理员</a>
-<div id="nav-admin" class="collapse in">
+<div id="nav-admin" class="collapse <?php if ( isset($_COOKIE['collapse_checkbox']) && $_COOKIE['collapse_checkbox'] == 'true') echo 'show'; else echo 'in'; ?>">
     <a class="nav-link" href="{{ route('admins.index') }}"><i class="iconfont icon-cc-dot-o"></i> 列表</a>
     <a class="nav-link" href="{{ route('roles.index') }}"><i class="iconfont icon-cc-dot-o"></i> 角色</a>
     <a class="nav-link" href="{{ route('permissions.index') }}"><i class="iconfont icon-cc-dot-o"></i> 权限</a>
 </div>
 
 <a class="nav-link" data-toggle="collapse" href="#nav-image"><i class="iconfont icon-picture-fill"></i> 图片资源</a>
-<div id="nav-image" class="collapse in">
+<div id="nav-image" class="collapse <?php if ( isset($_COOKIE['collapse_checkbox']) && $_COOKIE['collapse_checkbox'] == 'true') echo 'show'; else echo 'in'; ?>">
     <a class="nav-link" href="{{ route('image.index') }}"><i class="iconfont icon-cc-dot-o"></i> 所有图片</a>
     <a class="nav-link" href="{{ route('banner.index') }}"><i class="iconfont icon-cc-dot-o"></i> Banner 设定</a>
 </div>
